e395404d7c4eca0acd257e477c313538510308db
Pawel-Drabczyk <paweel.drabczyk@gmail.com>
2021-02-18 14:28:33 +0100

    initialization of this fork repo with local code from another private repo

---
+++ analyze.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# coding: utf-8
+import os
+import torch
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn.model_selection import train_test_split
+from pytorch_lightning.loggers.neptune import NeptuneLogger
+from pytorch_lightning.callbacks import LearningRateMonitor
+from datetime import datetime
+datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
+#custom components
+from networks import VeloDecoder, VeloEncoder, VeloAutoencoderLt
+from calibration_dataset import Tell1Dataset
+
+
+#trainig parameters
+PARAMS = {'max_epochs': 1,
+          'learning_rate': 0.02,
+          'batch_size': 64,
+          'gpus': 1,
+          'experiment_name': 'large-net normal-epochs standarized SGD no-dropout bigger-batches relu shuffle',
+          'tags': ['large-net', 'normal-epochs', 'standarized', 'SGD', 'no-dropout', 'bigger-batches', 'relu',
+                   'shuffle'],
+          'source_files': ['analyze_Pawel.py', 'networks.py']
+}
+
+datasetNames = ['dfh', 'dfhr', 'dfhphi', 'dfp', 'dfpr', 'dfpphi']
+
+
+class MyDS(Tell1Dataset):
+    filename_format = '%Y-%m-%d'
+    filename_regex_format = r'\d{4}-\d{2}-\d{2}.csv'
+
+
+#loading the data
+datapath = os.path.join("data", "calibrations")
+data_list = MyDS.get_filepaths_from_dir(datapath)
+mds = MyDS(data_list, read=True)
+
+dfh = mds.dfh.df.iloc[:, 9:]
+dfh_r = mds.dfh['R'].df.iloc[:, 9:]
+dfh_phi = mds.dfh['phi'].df.iloc[:, 9:]
+dfp = mds.dfp.df.iloc[:, 9:]
+dfp_r = mds.dfp['R'].df.iloc[:, 9:]
+dfp_phi = mds.dfp['phi'].df.iloc[:, 9:]
+
+
+dfh_metadata = mds.dfh.df.iloc[:, :9]
+dfh_r_metadata = mds.dfh['R'].df.iloc[:, :9]
+dfh_phi_metadata = mds.dfh['phi'].df.iloc[:, :9]
+dfp_metadata = mds.dfp.df.iloc[:, :9]
+dfp_r_metadata = mds.dfp['R'].df.iloc[:, :9]
+dfp_phi_metadata = mds.dfp['phi'].df.iloc[:, :9]
+
+#scaling input data
+dfh = dfh.sub(dfh.mean(1), axis=0).div(dfh.std(1), axis=0)
+dfh_r = dfh_r.sub(dfh_r.mean(1), axis=0).div(dfh_r.std(1), axis=0)
+dfh_phi = dfh_phi.sub(dfh_phi.mean(1), axis=0).div(dfh_phi.std(1), axis=0)
+dfp = dfp.sub(dfp.mean(1), axis=0).div(dfp.std(1), axis=0)
+dfp_r = dfp_r.sub(dfp_r.mean(1), axis=0).div(dfp_r.std(1), axis=0)
+dfp_phi = dfp_phi.sub(dfp_phi.mean(1), axis=0).div(dfp_phi.std(1), axis=0)
+
+
+def make_loader(dataset):
+    train, test = train_test_split(dataset, test_size=0.2)
+    train_target = torch.tensor(train.values, dtype=torch.float)
+    train_data = torch.tensor(train.values, dtype=torch.float)
+    test_target = torch.tensor(test.values, dtype=torch.float)
+    test_data = torch.tensor(test.values, dtype=torch.float)
+    train_tensor = TensorDataset(train_data, train_target)
+    test_tensor = TensorDataset(test_data, test_target)
+    train_loader = DataLoader(dataset=train_tensor, shuffle=True)
+    test_loader = DataLoader(dataset=test_tensor, shuffle=True)
+    return train_loader, test_loader
+
+
+def make_model_trainer(s, neptune_logger, lr):
+    #s = 2048
+    dec = VeloDecoder(s)
+    enc = VeloEncoder(s)
+    model = VeloAutoencoderLt(enc, dec, lr)
+    lr_monitor = LearningRateMonitor(logging_interval='step')
+    tr = pl.Trainer(logger=neptune_logger, callbacks=[lr_monitor],  max_epochs=PARAMS['max_epochs'],
+                    gpus=PARAMS['gpus'])
+    return model, tr
+
+
+def run_experiment(dataset, datasetName, par):
+    train_loader, test_loader = make_loader(dataset)
+    s = dataset.shape[1]
+    neptune_logger = NeptuneLogger(
+        api_key=os.getenv('NEPTUNE_API_TOKEN'),
+        project_name="pawel-drabczyk/velodimred",
+        experiment_name=par['experiment_name'],
+        params=par,
+        tags=par['tags'] + [datasetName],
+        upload_source_files=par['source_files']
+    )
+    model, tr = make_model_trainer(s, neptune_logger, par['learning_rate'])
+    tr.fit(model, train_loader, test_loader)
+
+    torch.save(model, os.path.join('models', PARAMS['experiment_name'], datasetName, "trained_model.ckpt"))
+    neptune_logger.experiment.log_artifact(os.path.join('models', PARAMS['experiment_name'], datasetName,
+                                                        "trained_model.ckpt"))
+
+
+if __name__ == "__main__":
+    for d in datasetNames:
+        if not os.path.exists(os.path.join('models', PARAMS['experiment_name'], d)):
+            os.makedirs(os.path.join('models', PARAMS['experiment_name'], d))
+
+    run_experiment(dfh, 'dfh', PARAMS)
+    #run_experiment(dfh_r, 'dfhr', PARAMS)
+    #run_experiment(dfh_phi, 'dfhphi', PARAMS)
+    #run_experiment(dfp, 'dfp', PARAMS)
+    #run_experiment(dfp_r, 'dfpr', PARAMS)
+    #run_experiment(dfp_phi, 'dfpphi', PARAMS)
---
+++ networks.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env jupyter
+
+#
+# Splits:
+# Dimentionality: [t, module, channel]
+#
+# Entire detector: x=t [module * channel, 1] - to little data
+# Only channels: x=t*module [channel, 1]
+# Only phi/R x=t*module/2 [channel, 1]
+
+# https://gitlab.cern.ch/mmajewsk/calina.git
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import pytorch_lightning as pl
+from calibration_dataset import Tell1Dataset
+
+
+class MyDS(Tell1Dataset):
+    filename_format = "%Y-%m-%d"
+    filename_regex_format = r"\d{4}-\d{2}-\d{2}.csv"
+
+
+def get_dataset(path):
+    data_list = MyDS.get_filepaths_from_dir(datapath)
+    mds = MyDS(data_list, read=True)
+    return mds
+
+
+class VeloEncoder(nn.Module):
+    def __init__(self, input_size):
+        nn.Module.__init__(self)
+        self.e1 = nn.Linear(input_size, 40)
+        self.e2 = nn.Linear(40, 10)
+        self.e3 = nn.Linear(10, 2)
+
+    def forward(self, x):
+        x = self.e1(x)
+        x = F.relu(x)
+        x = self.e2(x)
+        x = F.relu(x)
+        x = self.e3(x)
+        return x
+
+
+class VeloDecoder(nn.Module):
+    def __init__(self, output_size):
+        nn.Module.__init__(self)
+        self.e3 = nn.Linear(2, 10)
+        self.e2 = nn.Linear(10, 40)
+        self.e1 = nn.Linear(40, output_size)
+
+    def forward(self, x):
+        x = self.e3(x)
+        x = F.relu(x)
+        x = self.e2(x)
+        x = F.relu(x)
+        x = self.e1(x)
+
+        return x
+
+
+class VeloAutoencoderLt(pl.LightningModule):
+    def __init__(self, encoder=VeloEncoder(2048), decoder=VeloDecoder(2048), learning_rate=0.666):
+        pl.LightningModule.__init__(self)
+        self.enc = encoder
+        self.dec = decoder
+        self.lr = learning_rate
+
+    def forward(self, x):
+        x = self.enc(x)
+        x = self.dec(x)
+        return x
+
+    def configure_optimizers(self):
+        optimizer = optim.SGD(self.parameters(), lr=self.lr)
+        return optimizer
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.mse_loss(y_hat, y)
+        self.log("loss", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.mse_loss(y_hat, y)
+        self.log("val_loss", loss)
+        return loss
