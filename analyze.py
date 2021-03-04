6ee8011d7e828575f7959f00296da9e1dcfb1ab9
Tymek <tymoteusz@akcja.pl>
2021-03-04 17:34:04 +0100

    Added working demo of the project.

--- analyze.py
+++ analyze.py
@@ -16,11 +16,11 @@
 
 # <h1> VELO detector - dimensional reduction problem using autoencoders and PCA</h1>
 # <h3> Authors: Tymoteusz Ciesielski, Paweł Drabczyk, Aleksander Morgała </h3>
+# <h4> AGH UST 2020/2021 Faculty of Physics and Applied Computer Science</h4>
 # <h4> Project realised for the Python in the Enterprise course. </h4>
-# <h4> AGH UST 2020/2021 </h4>
 #
 
-# <h4>Introduction</h4>
+# <h3>1. Introduction</h3>
 # Our task was to reduce the number of dimensions in the VELO detector. Each dimension of the problem represents one sensor from 4096 present. We have applied two approaches:
 # <ol>
 #     <li> Principal Component Analysis (PCA) </li>
@@ -29,22 +29,25 @@
 #
 # <a href=https://lhcb-public.web.cern.ch/en/detector/VELO-en.html>Link to more information about VELO.</a>
 #
-# blablablablbalbalabala
+# In the code below, you can find the application of the autoencoders.
 #
 
-# <h4> Data </h4>
+# <h3> 2. Data </h3>
 # We were using the data from here:
 #
 # [insert some links etc]
+#
+# If you want to run the code, you should place the folder with the data in your working directory.
 
-# <h4> Technologies </h4>
+# <h3> 3. Technologies </h3>
 # <ul>
-#     <li> <b>Python</b> (PyTorch)</li>
+#     <li> <b>Python</b> (PyTorch) - provides the basic architecture for the neural networks we were using</li>
 #     <li> <b>Neptune AI </b> - to keep track of the training process, register networks architectures and save the parameters</li>
-#     <li> Pandas</li>
-#     <li> sth</li>
+#     <li> <b>Calina</b> library</li>
 # </ul>
 #     
+
+# <h3> 4. Code example </h3>
 
 import os
 import torch
@@ -60,8 +63,12 @@ import pandas as pd
 import neptune
 import zipfile
 
+# networks.py contains our custom architectures for autoencoders. They are built upon the pl.LightningModule
+
 from networks import VeloDecoder, VeloEncoder, VeloAutoencoderLt
 from calibration_dataset import Tell1Dataset
+
+# Below are all the necessary parameters for the training process, creating plots and saving them in Neptune.
 
 #trainig parameters
 PARAMS = {'max_epochs': 1,
@@ -71,14 +78,18 @@ PARAMS = {'max_epochs': 1,
           'experiment_name': 'small-net more-epochs standarized SGD no-dropout bigger-batches relu shuffle',
           'tags': ['small-net', 'normal-epochs', 'standarized', 'SGD', 'no-dropout', 'bigger-batches', 'relu', 'shuffle'],
           'source_files': ['small-net', 'more-epochs', 'standarized', 'SGD', 'no-dropout', 'bigger-batches', 'relu', 'shuffle'],
-          'experiment_id': 'VEL-372'
+          'experiment_id': 'VEL-398'
 }
 
+
+# Some help for extracting the data.
 
 class MyDS(Tell1Dataset):
     filename_format = '%Y-%m-%d'
     filename_regex_format = r'\d{4}-\d{2}-\d{2}.csv'
 
+
+# Loading the data from all soutces and channels. We also scale the input data.
 
 # +
 #loading the data
@@ -112,6 +123,8 @@ dfp_phi = dfp_phi.sub(dfp_phi.mean(1), axis=0).div(dfp_phi.std(1), axis=0)
 
 # -
 
+# Create loaders for the training.
+
 def make_loader(dataset):
     train, test = train_test_split(dataset, test_size=0.2)
     train_target = torch.tensor(train.values, dtype=torch.float)
@@ -125,6 +138,8 @@ def make_loader(dataset):
     return train_loader, test_loader
 
 
+# Create trainers for the network, with some set training parameters.
+
 def make_model_trainer(s, neptune_logger, lr):
     #s = 2048
     dec = VeloDecoder(s)
@@ -136,6 +151,7 @@ def make_model_trainer(s, neptune_logger, lr):
     return model, tr
 
 
+# Function creating the slider plot to see the evolution of autoencoder results
 
 def slider_plot(dataset, datasetName, metadata, model):
     reducedData = model.enc.forward(torch.tensor(dataset.values, dtype=torch.float))
@@ -151,6 +167,8 @@ def slider_plot(dataset, datasetName, metadata, model):
     fig.show()
     return fig
 
+
+# Function creating the plot being the result of the training. All the channels can be later turned on/off in a custom way
 
 def clustering_plot(dataset, datasetName, metadata, model):
     
@@ -169,6 +187,7 @@ def clustering_plot(dataset, datasetName, metadata, model):
     return fig
 
 
+# Function connects to Neptune, starts a new experiment there and starts running the training process for the network. It then creates all the plots and saves them in the Neptune as well.
 
 def run_experiment(dataset, datasetName, par, metadata):
     model_path = os.path.join('models', par['experiment_name'], datasetName)
@@ -204,6 +223,7 @@ def run_experiment(dataset, datasetName, par, metadata):
     neptune_logger.experiment.log_artifact(os.path.join(model_path, "clustering_plot.html"))
 
 
+# The function was used to reopen experiments, and add the cluster plots and slider plots, which was added at the end of the project.
 
 def reopen_experiment(dataset, datasetName, par, metadata):
     exp_id = par['experiment_id']
@@ -236,6 +256,7 @@ def reopen_experiment(dataset, datasetName, par, metadata):
     my_exp.append_tag('interactive')
 
 
+# The cell below runs te training process for all the datasets, for the current experiment, for the current network configuration.
 
 # +
 datasetNames = ['dfh', 'dfhr', 'dfhphi', 'dfp', 'dfpr', 'dfpphi']
@@ -248,9 +269,11 @@ datasetNames = ['dfh', 'dfhr', 'dfhphi', 'dfp', 'dfpr', 'dfpphi']
 #run_experiment(dfp_phi, 'dfpphi', PARAMS, dfp_phi_metadata)
 # -
 
+# The cell below opens existing experiment, adding the slider plots and cluster plots.
+
 #reopen_experiment(dfh, 'dfh', PARAMS, dfh_metadata)
 #reopen_experiment(dfh_r, 'dfhr', PARAMS, dfh_r_metadata)
-reopen_experiment(dfh_phi, 'dfhphi', PARAMS, dfh_phi_metadata)
+#reopen_experiment(dfh_phi, 'dfhphi', PARAMS, dfh_phi_metadata)
 #reopen_experiment(dfp, 'dfp', PARAMS, dfp_metadata)
 #reopen_experiment(dfp_r, 'dfpr', PARAMS, dfp_r_metadata)
-#reopen_experiment(dfp_phi, 'dfpphi', PARAMS, dfp_phi_metadata)
+reopen_experiment(dfp_phi, 'dfpphi', PARAMS, dfp_phi_metadata)
