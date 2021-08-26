8dd681af002a0e2c382f725d5a457395d266a4e8
Maciej Majewski <maciej.w.majewski@gmai.com>
2021-08-26 10:49:32 +0200

    fixes

--- analyze2.py
+++ analyze2.py
@@ -1,35 +1,3 @@
-# -*- coding: utf-8 -*-
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.11.4
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
-# <h1> Autoencoders</h1>
-
-# #### Installation
-#     
-# requirement.txt can be found in the repository. In order install all necessary packages you should run:
-#
-# pip install -r requirements.txt
-#
-# Adding Calina library to pythonpath is also necessary. If you are using using anaconda you can just type:
-#
-# conda develop Calina_path
-
-# #### Importing required modules
-
-# +
-
 import os
 import torch
 import pytorch_lightning as pl
@@ -44,7 +12,6 @@ import plotly.express as px
 import pandas as pd
 import neptune.new as neptune
 import zipfile
-# -
 
 # #### networks.py contains our custom architectures for autoencoders. They are built upon the pl.LightningModule
 
@@ -55,13 +22,13 @@ from calina_dataset.calibration_dataset import Tell1Dataset
 
 #trainig parameters
 PARAMS = {'max_epochs': 50,
-          'learning_rate': 0.05,
+          'learning_rate': 0.02,
           'batch_size': 16,
           'gpus': 1,
           'experiment_name': 'small-net more-epochs standarized SGD no-dropout bigger-batches relu shuffle',
           'tags': ['small-net', 'more-epochs', 'standarized', 'SGD', 'no-dropout', 'bigger-batches', 'relu', 'shuffle'],
           'source_files': ['analyze.ipynb', 'networks.py'],
-          'experiment_id':'VEL-371' 
+          'experiment_id': 'VEL-371'
 }
 
 
@@ -122,8 +89,8 @@ def make_loader(dataset):
     test_data = torch.tensor(test.values, dtype=torch.float)
     train_tensor = TensorDataset(train_data, train_target)
     test_tensor = TensorDataset(test_data, test_target)
-    train_loader = DataLoader(dataset=train_tensor, shuffle=True, batch_size=PARAMS['batch_size'])
-    test_loader = DataLoader(dataset=test_tensor, shuffle=True, batch_size=PARAMS['batch_size'])
+    train_loader = DataLoader(dataset=train_tensor, shuffle=True)
+    test_loader = DataLoader(dataset=test_tensor, shuffle=True)
     return train_loader, test_loader
 
 
@@ -145,7 +112,7 @@ def make_model_trainer(s, neptune_logger, lr):
 def slider_plot(dataset, datasetName, metadata, model):
     reducedData = model.enc.forward(torch.tensor(dataset.values, dtype=torch.float))
     reducedData = reducedData.detach().numpy()
-     
+
     indexesList = metadata.index.values.tolist()
     xyDF = pd.DataFrame(reducedData, index=indexesList, columns=['x', 'y'])
     resultDF = pd.concat([metadata, xyDF], axis=1)
@@ -162,17 +129,17 @@ def slider_plot(dataset, datasetName, metadata, model):
 # #### Function creating the plot being the result of the training. All the channels can be later turned on/off in a custom way
 
 def clustering_plot(dataset, datasetName, metadata, model):
-    
+
     reducedData = model.enc.forward(torch.tensor(dataset.values, dtype=torch.float))
     reducedData = reducedData.detach().numpy()
-     
+
     indexesList = metadata.index.values.tolist()
     xyDF = pd.DataFrame(reducedData, index=indexesList, columns=['x', 'y'])
     resultDF = pd.concat([metadata, xyDF], axis=1)
     resultDF["datetime"] = resultDF["datetime"].astype(str)
     resultDF["sensor"] = resultDF["sensor"].astype(str)
     print(resultDF)
-    
+
     fig = px.scatter(resultDF, x="x", y="y", color='sensor', opacity=0.5)
     fig.show(renderer="notebook")
     return fig
@@ -184,12 +151,12 @@ def run_experiment(dataset, datasetName, par, metadata):
     model_path = os.path.join('models', par['experiment_name'], datasetName)
     if not os.path.exists(model_path):
         os.makedirs(model_path)
-        
+
     train_loader, test_loader = make_loader(dataset)
     s = dataset.shape[1]
     neptune_logger = NeptuneLogger(
         api_key=os.getenv('NEPTUNE_API_TOKEN'),
-        project_name="pawel-drabczyk/velodimred",
+        project_name="mmajewsk/pubdimred",
         experiment_name=par['experiment_name'],
         params=par,
         tags=par['tags'] + [datasetName] + ['interactive'],
@@ -200,18 +167,18 @@ def run_experiment(dataset, datasetName, par, metadata):
 
     torch.save(model, os.path.join('models', PARAMS['experiment_name'], datasetName, "trained_model.ckpt"))
     neptune_logger.experiment.log_artifact(os.path.join(model_path, "trained_model.ckpt"))
-    
-    fig = slider_plot(dataset, datasetName, metadata, model)
-    fig.write_html(os.path.join(model_path, 'slider_plot.html'))
-    fig.write_image(os.path.join(model_path, 'slider_plot.png'))   
-    neptune_logger.experiment.log_image('slider_plot',os.path.join(model_path, 'slider_plot.png'))    
-    fig = clustering_plot(dataset, datasetName, metadata, model)
-    fig.write_html(os.path.join(model_path, 'clustering_plot.html'))
-    fig.write_image(os.path.join(model_path, 'clustering_plot.png'))    
-    neptune_logger.experiment.log_image('clustering_plot', os.path.join(model_path, 'clustering_plot.png'))        
-    
-    neptune_logger.experiment.log_artifact(os.path.join(model_path, "slider_plot.html"))
-    neptune_logger.experiment.log_artifact(os.path.join(model_path, "clustering_plot.html"))
+
+    # fig = slider_plot(dataset, datasetName, metadata, model)
+    # fig.write_html(os.path.join(model_path, 'slider_plot.html'))
+    # fig.write_image(os.path.join(model_path, 'slider_plot.png'))
+    # neptune_logger.experiment.log_image('slider_plot',os.path.join(model_path, 'slider_plot.png'))
+    # fig = clustering_plot(dataset, datasetName, metadata, model)
+    # fig.write_html(os.path.join(model_path, 'clustering_plot.html'))
+    # fig.write_image(os.path.join(model_path, 'clustering_plot.png'))
+    # neptune_logger.experiment.log_image('clustering_plot', os.path.join(model_path, 'clustering_plot.png'))
+
+    # neptune_logger.experiment.log_artifact(os.path.join(model_path, "slider_plot.html"))
+    # neptune_logger.experiment.log_artifact(os.path.join(model_path, "clustering_plot.html"))
 
 
 # #### The function was used to reopen experiments, and add the cluster plots and slider plots, which was added at the end of the project.
@@ -222,28 +189,27 @@ def reopen_experiment(dataset, datasetName, par, metadata):
     model_path = os.path.join('models', exp_name, datasetName)
     if not os.path.exists(model_path):
         os.makedirs(model_path)
-    
+
     run = neptune.init('pawel-drabczyk/velodimred', run=PARAMS['experiment_id'], mode="read-only", capture_stderr=False, capture_stdout=False)
     run['artifacts/trained_model.ckpt'].download()
-    run['source_code/files'].download()
     with zipfile.ZipFile("files.zip","r") as zip_ref:
         zip_ref.extractall('legacy_network')
-        
+
     import calina_dataset.calibration_dataset as calibration_dataset
     import sys
-    sys.modules['calibration_dataset'] = calibration_dataset  
+    sys.modules['calibration_dataset'] = calibration_dataset
 
     from legacy_network.networks import VeloDecoder, VeloEncoder, VeloAutoencoderLt
 
     model = torch.load(os.path.join(model_path,'trained_model.ckpt'), map_location=torch.device('cpu'))
-    
+
     fig = slider_plot(dataset, datasetName, metadata, model)
     fig.write_html(os.path.join(model_path, 'slider_plot.html'))
-    fig.write_image(os.path.join(model_path, 'slider_plot.png'))   
-  
+    fig.write_image(os.path.join(model_path, 'slider_plot.png'))
+
     fig = clustering_plot(dataset, datasetName, metadata, model)
     fig.write_html(os.path.join(model_path, 'clustering_plot.html'))
-    fig.write_image(os.path.join(model_path, 'clustering_plot.png'))    
+    fig.write_image(os.path.join(model_path, 'clustering_plot.png'))
 
 
 
@@ -253,7 +219,7 @@ def reopen_experiment(dataset, datasetName, par, metadata):
 datasetNames = ['dfh', 'dfhr', 'dfhphi', 'dfp', 'dfpr', 'dfpphi']
 
 #run_experiment(dfh, 'dfh', PARAMS, dfh_metadata)
-#run_experiment(dfh_r, 'dfhr', PARAMS, dfh_r_metadata)
+run_experiment(dfh_r, 'dfhr', PARAMS, dfh_r_metadata)
 #run_experiment(dfh_phi, 'dfhphi', PARAMS, dfh_phi_metadata)
 #run_experiment(dfp, 'dfp', PARAMS, dfp_metadata)
 #run_experiment(dfp_r, 'dfpr', PARAMS, dfp_r_metadata)
@@ -263,10 +229,8 @@ datasetNames = ['dfh', 'dfhr', 'dfhphi', 'dfp', 'dfpr', 'dfpphi']
 # #### The cell below opens existing experiment, adding the slider plots and cluster plots.
 
 #reopen_experiment(dfh, 'dfh', PARAMS, dfh_metadata)
-reopen_experiment(dfh_r, 'dfhr', PARAMS, dfh_r_metadata)
+# reopen_experiment(dfh_r, 'dfhr', PARAMS, dfh_r_metadata)
 #reopen_experiment(dfh_phi, 'dfhphi', PARAMS, dfh_phi_metadata)
 #reopen_experiment(dfp, 'dfp', PARAMS, dfp_metadata)
 #reopen_experiment(dfp_r, 'dfpr', PARAMS, dfp_r_metadata)
 #reopen_experiment(dfp_phi, 'dfpphi', PARAMS, dfp_phi_metadata)
-
-
