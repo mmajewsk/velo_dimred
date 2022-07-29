672d32b9e206980fa683ca9eee368565d7b93213
mmajewsk <maciej.w.majewski@gmail.com>
2022-07-29 10:12:05 +0200

    changes found

--- PCA.py
+++ PCA.py
@@ -6,9 +6,9 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.11.4
+#       jupytext_version: 1.10.1
 #   kernelspec:
-#     display_name: Python 3 (ipykernel)
+#     display_name: Python 3
 #     language: python
 #     name: python3
 # ---
@@ -36,7 +36,7 @@ import matplotlib as mpl
 
 # +
 
-from calina_dataset.calibration_dataset import Tell1Dataset
+from calina_dataset.calibration_dataset import Tell1Dataset, get_module_map
 
 class MyDS(Tell1Dataset):
     filename_format = '%Y-%m-%d'
@@ -47,6 +47,8 @@ data_list = MyDS.get_filepaths_from_dir(datapath)
 mds = MyDS(data_list, read=True)
 
 # -
+
+modmap = get_module_map()
 
 # ### Seperating data
 
@@ -62,6 +64,8 @@ for key in data:
                     'r_phi':data[key][data[key]['sensor_type'] == 'R']}
     for typ in data[key]:
         data[key][typ] = data[key][typ].drop(['mod_type','sensor_type'],axis=1)
+
+DATA_ = data.copy()
 
 # ### Importing modules needed for PCA
 
@@ -88,7 +92,19 @@ def set_color(plot):
 
 def full_pca(data,percent):
     data = StandardScaler().fit_transform(data)
+    #data_transponsed = data.transpose()
+    data_transponsed = data.copy()
+    pca = PCA(n_components = percent,svd_solver = 'auto')
+    result = pca.fit_transform(data_transponsed)
+    column_names = [f"Principal component {x+1}" for x in range(len(result[0]))]
+    row_names = [f"Channel {x}" for x in range(len(result))]
+    return pd.DataFrame(result,columns = column_names,index = row_names)
+
+
+def full_pca_trans(data,percent):
+    data = StandardScaler().fit_transform(data)
     data_transponsed = data.transpose()
+    #data_transponsed = data.copy()
     pca = PCA(n_components = percent,svd_solver = 'auto')
     result = pca.fit_transform(data_transponsed)
     column_names = [f"Principal component {x+1}" for x in range(len(result[0]))]
@@ -141,27 +157,32 @@ def do_a_pca_and_draw_a_plot(data):
 
 import plotly.express as px
 
-d = data['pedestal']['r_phi']
 
 
+def plotallped(t, trans=False):
+    d = DATA_['pedestal'][t]
+    single_data = {k: v.drop('sensor',axis=1) for k, v in d.groupby('sensor')}
+    alpha = 0.4
+    thisrun = []
+    for sensor_data_key in single_data:
+        dataset = single_data[sensor_data_key]
+        dataset_after_pca = full_pca(dataset,procentage_or_num_of_comp) if not trans else full_pca_trans(dataset, procentage_or_num_of_comp)
+        dataset_after_pca["sensor"] = str(int(sensor_data_key))
+        #scatter = plt.scatter(dataset_after_pca.iloc[:,0], dataset_after_pca.iloc[:,1], edgecolor='none', alpha=alpha,label=sensor_data_key)
+        #plt.legend(title="Module nr.")
+        thisrun.append(dataset_after_pca)
+    alldat = pd.concat(thisrun)
+    fig = px.scatter(alldat, x="Principal component 1", y="Principal component 2", color='sensor', opacity=0.5)
+    fig.show(renderer="notebook") 
+    fig.write_html("PCA_{}.html".format(t))
+    
+    return alldat, fig
 
-single_data = {k: v.drop('sensor',axis=1) for k, v in d.groupby('sensor')}
-alpha = 0.4
-thisrun = []
-for sensor_data_key in single_data:
-    dataset = single_data[sensor_data_key]
-    dataset_after_pca = full_pca(dataset,procentage_or_num_of_comp)
-    dataset_after_pca["sensor"] = str(int(sensor_data_key))
-    #scatter = plt.scatter(dataset_after_pca.iloc[:,0], dataset_after_pca.iloc[:,1], edgecolor='none', alpha=alpha,label=sensor_data_key)
-    #plt.legend(title="Module nr.")
-    thisrun.append(dataset_after_pca)
 
-alldat = pd.concat(thisrun)
+alldat, _ = plotallped('phi')
 
 # + pycharm={"name": "#%%\n"}
-fig = px.scatter(alldat, x="Principal component 1", y="Principal component 2", color='sensor', opacity=0.5)
-fig.show(renderer="notebook") 
-fig.write_html("PCA.html")
+alldat, _ = plotallped('r_phi')
 # -
 
 
@@ -248,6 +269,12 @@ fig.show(renderer="notebook")
 fig.write_html("PCA.html")
 fig.write_image("pics/PCA_module_R_all.png")
 
+fig = px.scatter(myd[myd['sensor_type']=='phi'], x="A", y="B", color='sensor', opacity=0.5)
+full_fig = fig.full_figure_for_development()
+fig.show(renderer="notebook") 
+fig.write_html("PCA.html")
+fig.write_image("pics/PCA_module_phi_all.png")
+
 full_fig.layout.xaxis.range
 
 fig = px.scatter(myd[myd['sensor_type']=='R'], x="A", y="B", color='sensor', opacity=0.5, animation_frame="timeplay")
@@ -256,17 +283,56 @@ fig.show(renderer="notebook")
 fig.write_html("PCA.html")
 
 
-getplotR
+def getanyplot(t):
+    return lambda x: px.scatter(myd[(myd['sensor_type']==t) & (myd['timeplay']==x)], x="A", y="B", color='sensor', opacity=0.5,)
+getplotR = getanyplot('R')
+getplotphi = getanyplot('phi')
 
-getplotR = lambda x: px.scatter(myd[(myd['sensor_type']=='R') & (myd['timeplay']==x)], x="A", y="B", color='sensor', opacity=0.5,)
+# +
+plotdates = [
+    '2012-04-27',
+    '2012-05-04',
+'2012-06-27',
+'2012-07-06',
+'2012-07-30',
+'2012-08-01',
+'2012-08-02',
+'2012-08-14',
+    '2012-09-23',
+    '2013-01-30'
+    ]
 
-import plotly.graph_objects as go
-def getplotR(dat):
-    partdat = myd[(myd['sensor_type']=='R') & (myd['timeplay']==dat)]
-    
-    rval = go.Scatter(x=partdat["A"], y=partdat["B"], fillcolor=partdat['sensor'], opacity=0.5)
-    return rval
 
+from plotly.subplots import make_subplots
+
+def maketogetherplot(t):
+    fig2 = px.scatter(myd[myd['sensor_type']==t], x="A", y="B", color='sensor', opacity=0.5)
+    full_fig = fig2.full_figure_for_development()
+    fig = make_subplots(rows=2, cols=5, shared_xaxes=True,  shared_yaxes=True, subplot_titles=plotdates)
+
+    for i, dat in enumerate(plotdates):
+        trc = getanyplot(t)(dat)
+        # fig.update_layout(xaxis=dict(range=full_fig.layout.xaxis.range),yaxis=dict(range=full_fig.layout.yaxis.range))
+        # fig.write_image("pics/NN_module_{}_{}.png".format(stype, i))
+
+        x = (i%5)+1
+        y = (i//5)+1
+        # trc = getplotR(date)
+        trc.update_layout(xaxis=dict(range=full_fig.layout.xaxis.range),yaxis=dict(range=full_fig.layout.yaxis.range), yaxis_title=dat)
+        fig.append_trace(trc['data'][0],col=x, row=y)
+        fig['layout']['yaxis{}'.format(i+1)]['range']=full_fig.layout.yaxis.range
+        fig['layout']['xaxis{}'.format(i+1)]['range']=full_fig.layout.xaxis.range
+    fig.update_layout( autosize=False, width=1200, height=600,)
+
+    fig.write_image("pics/PCA_module_{}_together.png".format(t))
+    fig.show()
+
+
+# -
+
+maketogetherplot("R")
+
+maketogetherplot("phi")
 
 # +
 from plotly.subplots import make_subplots
@@ -371,8 +437,89 @@ fig.write_html("PCA.html")
 
 
 
+datype = 'r_phi'
+alldat, figall = plotallped(datype, True)
+part1 = alldat.sensor.unique()[:11]
+part2 = alldat.sensor.unique()[11:21]
+part3 = alldat.sensor.unique()[21:32]
+part4 = alldat.sensor.unique()[32:]
+alldat1 = alldat[alldat.sensor.isin(part1)]
+alldat2 = alldat[alldat.sensor.isin(part2)]
+alldat3 = alldat[alldat.sensor.isin(part3)]
+alldat4 = alldat[alldat.sensor.isin(part4)]
+figall.write_image("pics/PCA_pedestals_all_{}.png".format(datype))
+
+
+
+def minmax(df):
+    ran=  max(df) - min(df)
+    half = ran/2
+    mid = min(df) + half
+    scale = 1.05
+    min_, max_ = mid - scale*half, mid + scale*half
+    return min_, max_
+
+
+xaxisrange = dict(range=minmax(alldat["Principal component 1"]))
+yaxisrange = dict(range=minmax(alldat["Principal component 2"]))
+
+
 # + pycharm={"name": "#%%\n"}
-fig = px.scatter(alldat, x="Principal component 1", y="Principal component 2", color='sensor', opacity=0.5)
-fig.show(renderer="notebook") 
-fig.write_html("PCA.html")
+def produce2plot(data, xaxisrange, yaxisrange):
+    fig = px.scatter(data, x="Principal component 1", y="Principal component 2", color='sensor', opacity=0.5, width=600, height=600)
+    fig.update_yaxes(
+        scaleanchor = "x",
+        scaleratio = 1,
+      )
+    fig.update_layout(xaxis=xaxisrange,yaxis=yaxisrange)
+    #fig.show(renderer="notebook") 
+    #fig.write_image("pics/PCA_pedestals.png")
+    return fig
+
+
+# -
+
+dats = [alldat1, alldat2, alldat3, alldat4]
+for i, d in enumerate(dats):
+    fig = produce2plot(d, xaxisrange, yaxisrange)
+    fig.write_image('pics/PCA_pedestals_{}_{}.png'.format(datype, i))
+
+alldat11
+
+selected_points = ['Channel 543','Channel 1899','Channel 322']
+
+alldat11.iloc[0,3]
+
+
+
+# + pycharm={"name": "#%%\n"}
+alldat11 = alldat2.copy()
+alldat11 = alldat2[alldat2.sensor == '11']
+alldat11.loc[selected_points, 'sensor'] = 'other'
+alldat11['alpha'] = 0.3
+alldat11.loc[selected_points, 'alpha'] = 1.0
+#fig.clear()
+fig = px.scatter(alldat11[alldat11.sensor == '11'], x="Principal component 1", y="Principal component 2", color='sensor', opacity=0.3, hover_name=alldat11[alldat11.sensor=='11'].index, width=600, height=600)
+# -
+
+fig, axe = plt.subplots(1,1, figsize=(6,6))
+axe.scatter(x=alldat11["Principal component 1"], y=alldat11["Principal component 2"], color='blue')
+alldsel = alldat11[alldat11.sensor == 'other']
+axe.scatter(x=alldsel["Principal component 1"], y=alldsel["Principal component 2"], color='red')
+for i, txt in enumerate(selected_points):
+    axe.annotate(txt, (alldsel.iloc[i, "Principal component 1"], alldsel.loc[i,"Principal component 1"]))
+axe.grid()
+
+fig, axe = plt.subplots(1,1,figsize=(6,3))
+for channel in selected_points:
+    stmpd[channel.lower().replace(' ', '')].plot(ax=axe)
+axe.legend()
+axe.legend(loc='upper center', bbox_to_anchor=(0.5, 1.16),
+          ncol=3, fancybox=True, shadow=True)
+fig.savefig("pics/PCA_trends_channel.png")
+
+
+
+
+
 
